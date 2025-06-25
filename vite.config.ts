d406359fdb1007c28f9f0142174da8ad311e177f
Jayanth Utukuri <jayanthutukuri2909@gmail.com>
2025-06-24 23:45:47 -0400

    Set base path in vite.config.ts for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // 👈 Add your repo name here
+  base: '/Portfolio/', // 👈 Add your repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
