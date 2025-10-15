3f907c41ca1309c0f48d1625f8beabc98343fd52
Jayanth Utukuri <jayanthutukuri2909@gmail.com>
2025-10-14 21:00:56 -0400

    Updates

--- assets/Education-uluvnsym.js
+++ assets/Education-uluvnsym.js
@@ -1,4 +1,4 @@
-import{f as n,j as e,m as i,T as a,C as d,a as m}from"./index-BRPYy2EJ.js";import{C as h,a as x}from"./calendar-DKv0VFK6.js";import"./createSvgIcon-xK1PfvVp.js";/**
+import{f as n,j as e,m as i,T as a,C as d,a as m}from"./index-C521qXU7.js";import{C as h,a as x}from"./calendar-D-ANDtX1.js";import"./createSvgIcon-jJQvXynP.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
